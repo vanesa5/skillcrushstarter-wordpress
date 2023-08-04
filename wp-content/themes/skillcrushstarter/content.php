571540eaeb1c7727d12fa888e6f6e9a5a9b0b7cd
Vanesa G <87158362+vanesa5@users.noreply.github.com>
2023-08-04 14:29:20 -0400

    Loading more posts section was added to access other post.

--- wp-content/themes/skillcrushstarter/content.php
+++ wp-content/themes/skillcrushstarter/content.php
@@ -13,6 +13,9 @@
 <article id="post-<?php the_ID(); ?>" class="post-entry">
 	<div class="entry-wrap">
 		<header class="entry-header">
+			<div class="entry-meta">
+				<time class="entry-time"><?php echo get_the_date(); ?></time> 
+			</div>
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		</header>
 		<div class="entry-summary">
@@ -21,6 +24,10 @@
 		<footer class="entry-footer">
 			<div class="entry-meta">
 				<span class="entry-terms comments author">
+					Written by <?php the_author(); ?> 
+					/ 
+					Posted in <?php the_category(', '); ?>
+    				/	
 					<a href="<?php comments_link(); ?>"><?php comments_number( '<span>No Comments</span>', '1 Comment', '% comments' ); ?></a>
 				</span>
 			</div>
--- wp-content/themes/skillcrushstarter/index.php
+++ wp-content/themes/skillcrushstarter/index.php
@@ -28,4 +28,14 @@ get_header(); ?>
 	<?php get_sidebar(); ?>
 </section>
 
+<nav id="navigation" class="container">
+			<div class="left"><?php next_post_link('&larr; <span>Older Posts</span>'); ?></div>
+			<div class="pagination">
+				<?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+					echo 'Page '.$paged.' of '$wp_query->max_num_pages;
+				?>
+			</div>
+			<div class="right"><?php previous_posts_link('<span>Newer Posts</span> &arr;'); ?></div>
+</nav>
+
 <?php get_footer(); ?>
